fb516ca100d28b6baf016c73ab12ffb40984ccf6
Gabrielle Falcão <gabriellefalcao13@outlook.com>
2023-10-31 15:26:09 -0300

    primeiro commit

---
+++ AULA_11/exerc008.js
@@ -0,0 +1,6 @@
+var vel = 120
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel>100){
+    console.log('Você ultrapassou a velocidade permitida.MULTADO!')}
+else{console.log('Você está na velocidade permitida. Continue..')}
+console.log('Dirija sempre usando cinto de segurança')
